e03bb954f5bdac34f70317acdd35fbbb7bc2264b
Cédric Sterckx <cedric_sterckx@hotmail.com>
2024-12-09 22:20:50 +0100

    day 2 part 1

--- day-2.ts
+++ day-2.ts
@@ -1,11 +1,46 @@
 import fs from 'node:fs';
 
-
 const reportData = fs.readFileSync('input-day2.txt', 'utf-8').split('\n');
 
-let safeReportCounts = 0;
+let countValidReports = 0;
 
-reportData.forEach(report => {
-    const levels = report.split('  ');
+for (let i = 0; i < reportData.length; i++) {
+    const levels: number[] = reportData[i].split(' ').map(item => parseInt(item.trim()));
+    if (isDecreasingSafe(levels)) {
+        countValidReports++;
+    } else if (isIncreasingSafe(levels)) {
+        countValidReports++;
+    }
+}
 
-});
+console.log(countValidReports);
+
+function isDecreasingSafe(input: number[]) {
+    let isSafe = true;
+    let prevNumber = input[0];
+
+    for (let i = 1; i < input.length; i++) {
+        if ((prevNumber > input[i]) && (Math.abs(input[i] - prevNumber) <= 3)) {
+            prevNumber = input[i];
+        } else {
+            isSafe = false;
+            break;
+        }
+    }
+    return isSafe;
+}
+
+function isIncreasingSafe(input: number[]) {
+    let isSafe = true;
+    let prevNumber = input[0];
+
+    for (let i = 1; i < input.length; i++) {
+        if ((prevNumber < input[i]) && (Math.abs(prevNumber - input[i]) <= 3)) {
+            prevNumber = input[i];
+        } else {
+            isSafe = false;
+            break;
+        }
+    }
+    return isSafe;
+}
